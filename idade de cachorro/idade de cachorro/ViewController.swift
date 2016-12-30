d77e2ca9b0bf9244de5fcc3f9f2eadedbf8e365b
Cesar Augusto <cesarkg6@Admins-Mac.local>
2016-12-30 21:31:16 -0200

    Primeiro commit

---
+++ idade de cachorro/idade de cachorro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  idade de cachorro
+//
+//  Created by Cesar Augusto on 30/12/16.
+//  Copyright © 2016 Cesar Augusto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

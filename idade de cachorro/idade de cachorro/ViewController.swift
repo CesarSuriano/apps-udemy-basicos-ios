a8fc0d580fbaad2cc0fa4c815ae96a957c2b7236
Cesar Augusto <cesarkg6@Admins-Mac.local>
2016-12-30 23:05:06 -0200

    Foi

--- idade de cachorro/idade de cachorro/ViewController.swift
+++ idade de cachorro/idade de cachorro/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var campoIdade: UITextField!
+    @IBOutlet weak var legendaResult: UILabel!
 
+    @IBAction func descobrirIdade(_ sender: UIButton) {
+        var idade = Int(campoIdade.text!)
+        idade = idade! * 7
+        
+        legendaResult.text = String(idade!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

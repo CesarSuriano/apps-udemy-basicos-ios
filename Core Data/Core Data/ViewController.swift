d43d193b09cf0eb9370a38efd1cac79b60397e41
Cesar Augusto <cesarkg6@Admins-Mac.local>
2017-06-02 23:39:28 -0300

    inserindo e listando produtos

--- Core Data/Core Data/ViewController.swift
+++ Core Data/Core Data/ViewController.swift
@@ -14,23 +14,88 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
         
-        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
-        usuario.setValue("Cesar Augusto", forKey: "nome")
-        usuario.setValue("cesar", forKey: "loginUsuario")
-        usuario.setValue(20, forKey: "idade")
-        usuario.setValue("123", forKey: "senha")
+        // --- Usuario Begin
+        //let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        //let context = appDelegate.persistentContainer.viewContext
         
-        do {
-            try
-            context.save()
-            print("Dados salvos com success")
-        }
-        catch {
-            print("Erro ao salvar")
-        }
+        
+        //Listar
+//        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Usuario")
+//        
+//        do {
+//            let usuarios = try context.fetch(requisicao)
+//            
+//            if usuarios.count > 0 {
+//                
+//                for usuario in usuarios {
+//                if let nomeUsuario = (usuario as AnyObject).value(forKey: "nome") {
+//                    print(nomeUsuario)
+//                }
+//                }
+//            } else {
+//                print("Não existem dados")
+//            }
+//            
+//        }catch {
+//            
+//        }
+        
+        //Salvar
+        
+//        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: context)
+//        usuario.setValue("Cesar Augusto", forKey: "nome")
+//        usuario.setValue("cesar3", forKey: "loginUsuario")
+//        usuario.setValue(20, forKey: "idade")
+//        usuario.setValue("123", forKey: "senha")
+//        
+//        do {
+//            try
+//            context.save()
+//            print("Dados salvos com success")
+//        }
+//        catch {
+//            print("Erro ao salvar")
+//        }
+//        -- usuarios end
+        
+        let appDelegateProduto = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegateProduto.persistentContainer.viewContext
+        
+//        let produto = NSEntityDescription.insertNewObject(forEntityName: "Produtos", into: context)
+//        produto.setValue("Arroz", forKey: "nome")
+//        produto.setValue("Arroz zaeli", forKey: "descricao")
+//        produto.setValue("branco", forKey: "cor")
+//        produto.setValue(10.90, forKey: "preco")
+//        
+//        do {
+//            try
+//            context.save()
+//            print("Dados salvos")
+//        } catch {
+//            print("Erro ao salvar")
+//        }
+        
+                let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Produtos")
+        
+                do {
+                    let produtos = try context.fetch(requisicao)
+        
+                    if produtos.count > 0 {
+        
+                        for produto in produtos {
+                        if let nomeProduto = (produto as AnyObject).value(forKey: "nome") {
+                            print(nomeProduto)
+                        }
+                        }
+                    } else {
+                        print("Não existem dados")
+                    }
+                    
+                }catch {
+                    
+                }
+
         
     }
 
